33188cb8c222aa09c00a49cac65c953d24502410
Cornes de veau <jean.jbizeul@gmail.com>
2024-09-13 11:37:52 +0200

    Added a link to Renaud's Website

--- app/(homePage)/page.tsx
+++ app/(homePage)/page.tsx
@@ -7,9 +7,15 @@ export default function Home() {
         </h1>
         <div className="ml-5 mt-5">
         <h2 className="text-2xl font-semibold">Le contenu du site.</h2>
-        <p>Ce site est un regroupement de divers projets React que j&apos;ai pu accomplir, il tourne grâce à Next.js (que je suis aussi en train d&apos;apprendre...).</p>
-        <p>Vous pouvez naviguer dans les différents projets via la barre de navigation située à votre gauche.</p>
-        <p>Bonne exploration !</p>
+        <p>
+          Ce site est un regroupement de divers projets React que j&apos;ai pu accomplir, il tourne grâce à Next.js (que je suis aussi en train d&apos;apprendre...). <br />
+          Vous pouvez naviguer dans les différents projets via la barre de navigation située à votre gauche. <br />
+          Bonne exploration !
+        </p>
+        <h2 className="text-2xl font-semibold">Les collègues :</h2>
+        <ul className="list-disc m-5">
+          <li><a className="text-blue-800 font-semibold" href='http://big-dev-soon-react-edition.vercel.app' target="_blank"><u>Chez Renaud.</u></a></li>
+        </ul>
       </div>
     </div>
   </main>
